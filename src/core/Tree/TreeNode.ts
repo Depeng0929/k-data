65d8695d16d026c8486f0cfcb4360311cb04eae2
depeng.kong <15365182986@163.com>
2021-08-02 17:45:10 +0800

    feat: tree

--- src/core/Tree/TreeNode.ts
+++ src/core/Tree/TreeNode.ts
@@ -1,3 +1,5 @@
+import { Queue } from '../../index'
+
 class TreeNode<T = unknown> {
   public children: TreeNode<T>[] = []
   public level: number
@@ -26,6 +28,22 @@ class TreeNode<T = unknown> {
         this.parent.children.splice(i, 1)
         return
       }
+    }
+  }
+
+  // 当前节点广度遍历
+  public breadthFirstSearch(callback: (p: any) => any = () => {}) {
+    const queue = new Queue<TreeNode>()
+    queue.enqueue(this)
+
+    while (!queue.isEmpty) {
+      const u = queue.dequeue()!
+      const children = u.children
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i]
+        queue.enqueue(child)
+      }
+      callback(u.val)
     }
   }
 }
