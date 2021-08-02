574807f71b0cbd32d11f709c94a55fda11fae28d
depeng.kong <15365182986@163.com>
2021-08-02 22:18:32 +0800

    refactory(stack): add doc

--- src/core/Stack/index.ts
+++ src/core/Stack/index.ts
@@ -2,6 +2,7 @@ import { deepClone } from '../../utils/index'
 
 class Stack<T = unknown> {
   private items: T[] = []
+
   constructor(
     items: T[] = [],
   ) {
@@ -12,16 +13,25 @@ class Stack<T = unknown> {
       throw new Error('传入的参数必须是数组')
   }
 
+  /**
+   * 当前栈的长度
+   * @return {number}
+   */
   get size() {
     return this.items.length
   }
 
+  /**
+   * 栈是否为空
+   * @return {boolean}
+   */
   get isEmpty() {
     return this.size === 0
   }
 
   /**
-   * 向栈中添加一个元素
+   * 入栈
+   * @param val {T} 入栈的元素
    */
   public push(val: T) {
     this.items.push(val)
@@ -29,20 +39,22 @@ class Stack<T = unknown> {
 
   /**
    * 出栈
+   * @return {T}
    */
   public pop() {
     return this.items.pop()
   }
 
   /**
-   * 查看栈顶第一个元素
+   * 返回栈顶的元素
+   * @return T
    */
   public peek() {
     return this.items[this.items.length - 1]
   }
 
   /**
-   * 清除栈中元素
+   * 清空栈
    */
   public clear() {
     return this.items = []
