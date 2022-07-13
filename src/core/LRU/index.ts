8ed11d09a6b8307a29bfbe9c20f9957e2892e2dd
depeng.kong <15365182986@163.com>
2022-07-13 22:45:27 +0800

    tmp

--- src/core/LRU/index.ts
+++ src/core/LRU/index.ts
@@ -1,27 +1,39 @@
+import type { ICompareFn } from '../../types'
+import { defaultCompareFn } from '../../utils'
 import LinkedList from '../LinkedList'
 
 interface LRUOptions<T = unknown> {
   max?: number
+  compareFn: ICompareFn<T>
 }
 
 export class LRU<V = unknown, K = string> {
   private max = 1000
-  private map = new Map<K, V>()
-  private linkList = new LinkedList<V>()
+  private compareFn = defaultCompareFn
+  private hashMap = new Map<K, V>()
+  private linkList = new LinkedList<V>({
+    compareFn: this.compareFn,
+  })
 
   constructor(
     options?: LRUOptions,
   ) {
-    const { max = 1000 } = options || {}
+    const { max = 1000, compareFn = defaultCompareFn } = options || {}
 
     this.max = max
+    this.compareFn = compareFn
   }
 
-  get Size() {
-    return this.map.size
+  get size() {
+    return this.hashMap.size
   }
 
   set(key: K, val: V) {
+    if (this.hashMap.has(key)) {
+      this.hashMap.delete(key)
+      this.linkList.remove(val)
+    }
+    this.hashMap.set(key, val)
   }
 
   get() {}
