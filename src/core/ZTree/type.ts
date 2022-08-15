e0b7b7b5833dc9adfa697eded9724750094c8fbf
depeng.kong <15365182986@163.com>
2022-08-15 23:10:33 +0800

    fix

--- src/core/ZTree/type.ts
+++ src/core/ZTree/type.ts
@@ -1,7 +1,7 @@
 export type RawListItem<K extends string, P extends string> = {
-  [id in K]: string | number;
+  [id in K]: string;
 } & {
-  [pid in P]: string | number;
+  [pid in P]: string;
 } & Record<string, unknown>
 
 export type ITreeListITem<K extends string, P extends string> = RawListItem<K, P> & {
